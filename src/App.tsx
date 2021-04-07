2da3179b77557b522001d4d404d6cfdd5987cadf
Henrique Domanski <henrydomanski@gmail.com>
2021-04-07 01:47:53 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       
+      <ExperienceBar/>
     </div>
   );
 }
 
 export default App;
 
-//teste
+
